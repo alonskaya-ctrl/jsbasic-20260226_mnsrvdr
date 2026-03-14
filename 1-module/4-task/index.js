86dd1be022de0e32a5d09dd89fc7f49a09826b9c
alonskaya-ctrl <mnsrvdr@gmail.com>
2026-03-14 14:16:22 +0300

    Done 1-module-4-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,18 @@
 function checkSpam(str) {
-  // ваш код...
+
+  let lowerStr = str.toLowerCase();
+
+  if (lowerStr.includes('1xbet') || lowerStr.includes('xxx')) {
+    return true;
+  }
+
+  return false;
 }
+console.log(checkSpam('1XbeT now')); 
+// true
+
+console.log(checkSpam('free xxxxx')); 
+// true
+
+console.log(checkSpam('innocent rabbit')); 
+// false
