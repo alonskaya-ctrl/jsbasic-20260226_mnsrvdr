869189740d275ab91574e45c75a3d5d1aadf549c
alonskaya-ctrl <mnsrvdr@gmail.com>
2026-03-15 15:28:06 +0300

    Added required fields

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,28 +1,39 @@
+let users = [
+  {
+    "balance": "$1,825.65",
+    "picture": "https://placehold.it/32x32",
+    "age": 21,
+    "name": "Golden Branch",
+    "gender": "male",
+    "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+    "favouriteFruit": "banana"
+  },
+  {
+    "balance": "$1,490.15",
+    "picture": "https://placehold.it/32x32",
+    "age": 25,
+    "name": "Duncan Randall",
+    "gender": "male",
+    "greeting": "Hello, Duncan Randall! You have 3 unread messages.",
+    "favouriteFruit": "apple"
+  },
+  {
+    "balance": "$2,500.00",
+    "picture": "https://placehold.it/32x32",
+    "age": 30,
+    "name": "Alice Cooper",
+    "gender": "female",
+    "greeting": "Hello, Alice Cooper! You have 5 unread messages.",
+    "favouriteFruit": "strawberry"
+  }
+];
+
 function showSalary(users, age) {
   return users
     .filter(user => user.age <= age)
     .map(user => `${user.name}, ${user.balance}`)
     .join('\n');
 }
-
-// Пример использования:
-let users = [
-  {
-    "balance": "$1,825.65",
-    "age": 21,
-    "name": "Golden Branch"
-  },
-  {
-    "balance": "$1,490.15",
-    "age": 25,
-    "name": "Duncan Randall"
-  },
-  {
-    "balance": "$2,500.00",
-    "age": 30,
-    "name": "Alice Cooper"
-  }
-];
 
 console.log(showSalary(users, 25));
 /* Вывод:
