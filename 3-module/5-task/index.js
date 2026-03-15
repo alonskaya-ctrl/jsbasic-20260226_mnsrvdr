91d13826cbb0449418a98e7c166c4ed7cf9552d9
alonskaya-ctrl <mnsrvdr@gmail.com>
2026-03-15 15:33:55 +0300

    Done 3-module-5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
-  // ваш код...
+  let items = str.split(' ');
+
+  let numbers = items
+    .map(item => Number(item))
+    .filter(item => !isNaN(item));
+
+  let min = Math.min(...numbers);
+  let max = Math.max(...numbers);
+
+  return { min, max };
 }
+
+// Пример использования
+let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+console.log(getMinMax(inputData)); 
+// { min: -5.8, max: 73 }
